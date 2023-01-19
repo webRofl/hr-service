2feac89a52d04b26118f7ae5b09ede2e07e3ac12
webRofl <maltsev_fe@mail.ru>
2023-01-19 23:39:42 +0400

    Remove views

--- backend/projects/urls.py
+++ backend/projects/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.projects, name="projects"),
-    path('project-object/<str:pk>/', views.project, name="project"),
-]
+urlpatterns = []
--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,18 +1,2 @@
 from django.shortcuts import render
 
-def projects(request):
-    projectsList = [{'id':'1',
-    'title':'Онлайн-кинотеатр',
-    'description':'Кинотеатр с самой полной библиотекой фильмов.'},
-    {'id':'2',
-    'title':'Платформа с ИТ-курсами',
-    'description':'Курсы по фронтенду, бэкенду и мобильной разработке.'},
-    {'id':'3',
-    'title':'Рекрутинговый портал',
-     'description':'Вакансии для специалистов экстра-класса.'},
-    ]
-      
-    return render(request, 'projects/projects.html', {'projects':projectsList})
-
-def project(request):
-    return render(request, 'projects/single-project.html')
