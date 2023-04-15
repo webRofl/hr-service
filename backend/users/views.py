41645cfcfb89e8650ec69bc686638118ae5d37f7
webRofl <maltsev_fe@mail.ru>
2023-04-16 03:34:47 +0400

    feat: edit users viewsets to formdata so was possibility set image

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,91 +1,76 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, mixins
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework.parsers import MultiPartParser, FormParser
 
-from .serializers import SkillSerializer, EmployeeProfileSerializer, EmployeeProfileListSerializer, EmployeeProfilePostSerializer, EmployeeProfileUpdateSerializer, EmployerProfileRetrieveSerializer, EmployerProfileListSerializer, EmployerProfilePostSerializer, EmployerProfileUpdateSerializer
+from .serializers import SkillSerializer, EmployeeProfileSerializer, EmployeeProfileListSerializer, EmployeeProfilePostSerializer, EmployeeProfileUpdateSerializer, EmployerProfileRetrieveSerializer, EmployerProfilePostSerializer, EmployerProfileUpdateSerializer
 from .models import Skill, EmployeeProfile, EmployerProfile
 from .permissions import IsGetMethodOrAuthOnly
 from users.models import User
 from authentication.models import User
 from helpers.get_data_with_user import get_data_with_user
 from helpers.permissions import is_owner
-from rest_framework.exceptions import PermissionDenied
+from helpers.unwrap_formdata_string import unwrap_formdata_string
+from helpers.serializer_lifecycle import serializer_lifecycle
 
-class EmployeeProfileCRUDViewSet(viewsets.ModelViewSet):
+# class EmployeeProfileCRUDViewSet(viewsets.ModelViewSet):
+#     queryset = EmployeeProfile.objects.all()
+#     serializer_class = EmployeeProfileSerializer
+#     permission_classes = (IsGetMethodOrAuthOnly,)
+#     http_method_names = ['get', 'post', 'put']
+
+#     def retrieve(self, request, pk=None):
+#         user = get_object_or_404(self.queryset.filter(user_id=pk))
+#         serializer = self.serializer_class(user, context={'request': request})
+
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+#     def list(self, request):
+#         serializer = EmployeeProfileListSerializer(self.get_queryset(), many=True, context={'request': request})
+
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+#     def update(self, request, pk=None):
+#         data = get_data_with_user(request)
+
+#         serializer = EmployeeProfileUpdateSerializer(data=data, instance=self.queryset.get(user=pk))
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+#     def create(self, request):
+#         data = get_data_with_user(request)
+#         user_instance = User.objects.get(id=data['user'])
+
+#         user_data = {
+#                 'email': user_instance.email,
+#                 'username': user_instance.username,
+#                 }
+
+#         data.update(user_data)
+
+#         serializer = EmployeeProfilePostSerializer(data=data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class EmployeeProfilePostView(mixins.CreateModelMixin,
+                              mixins.UpdateModelMixin,
+                              viewsets.GenericViewSet):
     queryset = EmployeeProfile.objects.all()
-    serializer_class = EmployeeProfileSerializer
+    serializer_class = EmployeeProfilePostSerializer
     permission_classes = (IsGetMethodOrAuthOnly,)
-    http_method_names = ['get', 'post', 'put', 'delete']
-
-    def retrieve(self, request, pk=None):
-        user = get_object_or_404(self.queryset.filter(user_id=pk))
-        serializer = self.serializer_class(user, context={'request': request})
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def list(self, request):
-        serializer = EmployeeProfileListSerializer(self.get_queryset(), many=True, context={'request': request})
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def update(self, request, pk=None):
-        data = get_data_with_user(request)
-
-        serializer = EmployeeProfileUpdateSerializer(data=data, instance=self.queryset.get(user=pk))
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    parser_classes = (MultiPartParser, FormParser)
 
     def create(self, request):
-        data = get_data_with_user(request)
-        user_instance = User.objects.get(id=data['user'])
-
-        user_data = {
-                'email': user_instance.email,
-                'username': user_instance.username,
-                }
-
-        data.update(user_data)
-
-        serializer = EmployeeProfilePostSerializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class EmployerProfileCRUDViewSet(viewsets.ModelViewSet):
-    queryset = EmployerProfile.objects.all()
-    serializer_class = EmployerProfileRetrieveSerializer
-    permission_classes = (IsGetMethodOrAuthOnly,)
-    http_method_names = ['get', 'post', 'put', 'delete']
-
-    def retrieve(self, request, pk=None):
         if not is_owner(request):
             return Response({}, status=status.HTTP_403_FORBIDDEN)
 
-        user = get_object_or_404(self.queryset.filter(user_id=pk))
-        serializer = self.serializer_class(user, context={'request': request})
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def list(self, request):
-        serializer = EmployerProfileListSerializer(self.get_queryset(), many=True, context={'request': request})
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def update(self, request, pk=None):
         data = get_data_with_user(request)
-
-        serializer = EmployerProfileUpdateSerializer(data=data, instance=self.queryset.get(user=pk))
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def create(self, request):
-        data = get_data_with_user(request)
+        data = unwrap_formdata_string(data)
         user_instance = User.objects.get(id=data['user'])
 
         user_data = {
@@ -95,11 +80,96 @@ class EmployerProfileCRUDViewSet(viewsets.ModelViewSet):
 
         data.update(user_data)
 
-        serializer = EmployerProfilePostSerializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        serializer = serializer_lifecycle(EmployeeProfilePostSerializer, data)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def update(self, request, pk=None):
+        if not is_owner(request):
+            return Response({}, status=status.HTTP_403_FORBIDDEN)
+
+        data = get_data_with_user(request)
+        data = unwrap_formdata_string(data)
+
+        serializer = serializer_lifecycle(EmployeeProfileUpdateSerializer, data, instance=self.queryset.get(user=pk))
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class EmployeeProfileReceiveView(mixins.RetrieveModelMixin,
+                                 viewsets.GenericViewSet):
+    queryset = EmployeeProfile.objects.all()
+    serializer_class = EmployeeProfileSerializer
+    permission_classes = (IsGetMethodOrAuthOnly,)
+
+    def retrieve(self, request, pk=None):
+        user = get_object_or_404(self.queryset.filter(user_id=pk))
+        serializer = self.serializer_class(user, context={'request': request})
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+class EmployeeProfileListView(mixins.ListModelMixin,
+                              viewsets.GenericViewSet):
+    queryset = EmployeeProfile.objects.all()
+    serializer_class = EmployeeProfileListSerializer
+
+    def list(self, request):
+        serializer = self.serializer_class(self.get_queryset(), many=True, context={'request': request})
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class EmployerProfilePostView(mixins.CreateModelMixin,
+                              mixins.UpdateModelMixin,
+                              viewsets.GenericViewSet):
+    queryset = EmployerProfile.objects.all()
+    serializer_class = EmployerProfilePostSerializer
+    permission_classes = (IsGetMethodOrAuthOnly,)
+    parser_classes = (MultiPartParser, FormParser)
+
+    def create(self, request):
+        if not is_owner(request):
+            return Response({}, status=status.HTTP_403_FORBIDDEN)
+
+        data = get_data_with_user(request)
+        data = unwrap_formdata_string(data)
+        user_instance = User.objects.get(id=data['user'])
+
+        user_data = {
+                'email': user_instance.email,
+                'username': user_instance.username,
+                }
+
+        data.update(user_data)
+
+        serializer = serializer_lifecycle(EmployerProfilePostSerializer, data)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def update(self, request, pk=None):
+        if not is_owner(request):
+            return Response({}, status=status.HTTP_403_FORBIDDEN)
+
+        data = get_data_with_user(request)
+        data = unwrap_formdata_string(data)
+
+        serializer = serializer_lifecycle(EmployerProfileUpdateSerializer, data, instance=self.queryset.get(user=pk))
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class EmployerProfileReceiveView(mixins.RetrieveModelMixin,
+                                 viewsets.GenericViewSet):
+    queryset = EmployerProfile.objects.all()
+    serializer_class = EmployerProfileRetrieveSerializer
+    permission_classes = (IsGetMethodOrAuthOnly,)
+
+    def retrieve(self, request, pk=None):
+        user = get_object_or_404(self.queryset.filter(user_id=pk))
+        serializer = self.serializer_class(user, context={'request': request})
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class SkillsCRUDViewSet(viewsets.ModelViewSet):
